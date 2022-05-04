347493062ede4e2fb012f5f664789bc9ba806872
alcabmo <alejandro.cabrera.dam@gmail.com>
2022-05-04 10:57:07 +0200

    tiene fallos

--- src/attention_main.cpp
+++ src/attention_main.cpp
@@ -2,6 +2,11 @@
 
 int main(int argc, char ** argv)
 {
-  rclcpp::init(argc, argv);
-
+    rclcpp::init(argc, argv);
+    auto graph_node = std::make_shared<GazeboGraph>();
+    auto node_neck = std::make_shared<NeckController>();
+    rclcpp::spin(graph_node);
+    rclcpp::spin(node_neck);
+    rclcpp::shutdown();
+    return 0;
 }
--- src/attention_roboros/GazeboGraph.cpp
+++ src/attention_roboros/GazeboGraph.cpp
@@ -3,6 +3,7 @@
 #include "std_msgs/msg/float64.hpp"
 
 #include "ros2_knowledge_graph/GraphNode.hpp"
+#include "gazebo_msgs/msg/ModelStates"
 
 class GazeboGraph : public rclcpp::Node
 {
@@ -11,9 +12,18 @@ class GazeboGraph : public rclcpp::Node
         GazeboGraph() : Node("gazebo_graph") {
             graph_ = std::make_shared<ros2_knowledge_graph::GraphNode>("node_A");
             graph_->start();
+            subscription_gazebo_ = this->create_subscription<gazebo_msgs::msg::ModelStates>(
+            "/gazebo/model_states", 10, std::bind(&GazeboGraph::callback, this, _1));
         }
 
     
     private:
         std::shared_ptr<ros2_knowledge_graph::GraphNode> graph_;
+        rclcpp::Subscription<gazebo_msgs::msg::ModelStates>::SharedPtr subscription_gazebo_;
+
+
+        void callback(const gazebo_msgs::msg::ModelStates::SharedPtr msg) const
+        {
+            RCLCPP_INFO("callback");
+        }
 };
