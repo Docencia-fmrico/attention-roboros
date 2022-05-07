72b5d706b275279ce526ff2680a0068447509689
alcabmo <alejandro.cabrera.dam@gmail.com>
2022-05-07 11:31:52 +0200

    no se por que no cpmpila peroe s una idea de como utilizar graph con funciones en nuestro nodo

--- src/attention_main.cpp
+++ src/attention_main.cpp
@@ -1,12 +1,9 @@
 #include "rclcpp/rclcpp.hpp"
-#include "attention_roboros/GazeboGraph.cpp"
 
 int main(int argc, char ** argv)
 {
     rclcpp::init(argc, argv);
-    auto graph_node = std::make_shared<GazeboGraph>();
     //auto node_neck = std::make_shared<NeckController>();
-    rclcpp::spin(graph_node);
     //rclcpp::spin(node_neck);
     rclcpp::shutdown();
     return 0;
--- src/attention_roboros/GazeboGraph.cpp
+++ src/attention_roboros/GazeboGraph.cpp
@@ -1,29 +1,63 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "std_msgs/msg/float64.hpp"
+#include "rclcpp/rclcpp.hpp"
 
+#include "ros2_knowledge_graph_msgs/msg/graph.hpp"
+#include "ros2_knowledge_graph_msgs/msg/graph_update.hpp"
+#include "ros2_knowledge_graph_msgs/msg/node.hpp"
+#include "ros2_knowledge_graph_msgs/msg/edge.hpp"
+
+#include "ros2_knowledge_graph/graph_utils.hpp"
 #include "ros2_knowledge_graph/GraphNode.hpp"
-#include "gazebo_msgs/msg/model_state.hpp"
+
 
 class GazeboGraph : public rclcpp::Node
 {
 
     public:
         GazeboGraph() : Node("gazebo_graph") {
-            graph_ = std::make_shared<ros2_knowledge_graph::GraphNode>("gazebo_graph");
-            //graph_->start();
-            subscription_gazebo_ = this->create_subscription<gazebo_msgs::msg::ModelState>(
-            "/gazebo/model_states", 10, std::bind(&GazeboGraph::callback, this, std::placeholders::_1));
+
+            //subscription_gazebo_ = this->create_subscription<gazebo_msgs::msg::ModelState>(
+            //"/gazebo/model_states", 10, std::bind(&GazeboGraph::callback, this, std::placeholders::_1));
+        }
+
+        void GazeboGraph::add_node(std::string & name,std::string % type){
+            if (command.size() == 2) {
+                auto node = ros2_knowledge_graph::new_node(name, type);
+                graph_->update_node(node);
+            } else {
+                RCLCPP_INFO(get_logger(),"Usage:add node [name] [type] ");
+            }
+        }
+
+        void GazeboGraph::add_edge(std::string  & source,std::string & target,std::string & target.std::string & type,std::string & target)
+        {          
+
+            auto edge = ros2_knowledge_graph::get_edge(source, target, type, data);
+
+            if (edge.has_value()) {
+                graph_->update_edge(edge.value());
+            } else {
+                RCLCPP_INFO(get_logger(),"Could not add the edge ");
+            }
+        }
+
+        void GazeboGraph::remove_node(std::string  & source)
+        {          
+            if (!graph_->remove_node(source)) {
+                RCLCPP_INFO(get_logger(),"Could not remove the node");
+            }
         }
 
     
     private:
         std::shared_ptr<ros2_knowledge_graph::GraphNode> graph_;
-        rclcpp::Subscription<gazebo_msgs::msg::ModelState>::SharedPtr subscription_gazebo_;
+        //rclcpp::Subscription<gazebo_msgs::msg::ModelState>::SharedPtr subscription_gazebo_;
 
 
-        void callback(const gazebo_msgs::msg::ModelState::SharedPtr msg) const
+        /*void callback(const gazebo_msgs::msg::ModelState::SharedPtr msg) const
         {
             RCLCPP_INFO(get_logger(),"callback");
-        }
+        }*/
 };
