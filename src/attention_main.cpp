5665542e1ddbe9c54e03c6912541cc2116fd6b07
alcabmo <alejandro.cabrera.dam@gmail.com>
2022-05-09 11:52:43 +0200

    Compila, clase cliente de knowledge_graph, falta el remove edge, traducir para lo de gazebo, ha dejado de ser nodo

--- src/attention_main.cpp
+++ src/attention_main.cpp
@@ -1,8 +1,12 @@
 #include "rclcpp/rclcpp.hpp"
 
+#include "attention_roboros/GazeboGraph.cpp"
+
 int main(int argc, char ** argv)
 {
     rclcpp::init(argc, argv);
+    auto node_neck = std::make_shared<GazeboGraph>();
+    //rclcpp::spin(node_neck);
     //auto node_neck = std::make_shared<NeckController>();
     //rclcpp::spin(node_neck);
     rclcpp::shutdown();
--- src/attention_roboros/GazeboGraph.cpp
+++ src/attention_roboros/GazeboGraph.cpp
@@ -12,52 +12,38 @@
 #include "ros2_knowledge_graph/GraphNode.hpp"
 
 
-class GazeboGraph : public rclcpp::Node
+class GazeboGraph
 {
 
     public:
-        GazeboGraph() : Node("gazebo_graph") {
 
-            //subscription_gazebo_ = this->create_subscription<gazebo_msgs::msg::ModelState>(
-            //"/gazebo/model_states", 10, std::bind(&GazeboGraph::callback, this, std::placeholders::_1));
+        void add_node(std::string name,std::string type){
+
+            auto node = ros2_knowledge_graph::new_node(name, type);
+            graph_->update_node(node);
+
+            std::cout << "Nodo añadido: " << name << "\n";
+
         }
 
-        void GazeboGraph::add_node(std::string & name,std::string % type){
-            if (command.size() == 2) {
-                auto node = ros2_knowledge_graph::new_node(name, type);
-                graph_->update_node(node);
-            } else {
-                RCLCPP_INFO(get_logger(),"Usage:add node [name] [type] ");
-            }
-        }
-
-        void GazeboGraph::add_edge(std::string  & source,std::string & target,std::string & target.std::string & type,std::string & target)
+        void add_edge(std::string source,std::string target,std::string type)
         {          
 
-            auto edge = ros2_knowledge_graph::get_edge(source, target, type, data);
+            auto edge = ros2_knowledge_graph::new_edge(source, target, type);
 
-            if (edge.has_value()) {
-                graph_->update_edge(edge.value());
-            } else {
-                RCLCPP_INFO(get_logger(),"Could not add the edge ");
-            }
+            graph_->update_edge(edge);
+            std::cout << "Edge añadido entre " << source << "y" << target << "\n";
+
         }
 
-        void GazeboGraph::remove_node(std::string  & source)
+        void remove_node(std::string source)
         {          
             if (!graph_->remove_node(source)) {
-                RCLCPP_INFO(get_logger(),"Could not remove the node");
+                std::cout << "No pudo remover edge: "<<source<< "\n";
             }
         }
 
     
     private:
         std::shared_ptr<ros2_knowledge_graph::GraphNode> graph_;
-        //rclcpp::Subscription<gazebo_msgs::msg::ModelState>::SharedPtr subscription_gazebo_;
-
-
-        /*void callback(const gazebo_msgs::msg::ModelState::SharedPtr msg) const
-        {
-            RCLCPP_INFO(get_logger(),"callback");
-        }*/
 };
